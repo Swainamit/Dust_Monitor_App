5407ee726613451a4cc5b4f58ec42c477cdfde73
Swainamit <swainamit18is@gmail.com>
2018-08-17 12:03:23 +0530

    commit_1

---
+++ DustComm/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea6 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend6 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series6 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Thistle
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1146, 263)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(170, 48)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Thistle
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1146, 328)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(170, 51)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Disconnect"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Thistle
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(1146, 393)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(170, 51)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Refresh"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.IndianRed
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(1146, 459)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(170, 50)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'SerialPort1
+        '
+        '
+        'Chart1
+        '
+        Me.Chart1.BorderlineColor = System.Drawing.Color.Black
+        Me.Chart1.BorderlineWidth = 5
+        Me.Chart1.BorderSkin.BackColor = System.Drawing.Color.Black
+        Me.Chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent
+        Me.Chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Raised
+        ChartArea6.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea6)
+        Legend6.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend6)
+        Me.Chart1.Location = New System.Drawing.Point(51, 263)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry
+        Series6.ChartArea = "ChartArea1"
+        Series6.Legend = "Legend1"
+        Series6.Name = "Series1"
+        Me.Chart1.Series.Add(Series6)
+        Me.Chart1.Size = New System.Drawing.Size(1069, 372)
+        Me.Chart1.TabIndex = 4
+        Me.Chart1.Text = "Chart1"
+        '
+        'Timer1
+        '
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.TextBox1.Font = New System.Drawing.Font("Calibri", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(1146, 559)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(183, 66)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.TextBox2.Font = New System.Drawing.Font("Calibri", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(1146, 186)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(170, 53)
+        Me.TextBox2.TabIndex = 6
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownWidth = 10
+        Me.ComboBox1.Font = New System.Drawing.Font("Calibri", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(51, 177)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(211, 67)
+        Me.ComboBox1.TabIndex = 8
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownWidth = 10
+        Me.ComboBox2.Font = New System.Drawing.Font("Calibri", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"2400 ", "4800 ", "9600 ", "19200", "57600", "115200"})
+        Me.ComboBox2.Location = New System.Drawing.Point(303, 177)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(212, 67)
+        Me.ComboBox2.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(1145, 538)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(184, 18)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Dust Concentration (ug/m3):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(1148, 165)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 19)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Device ID: "
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Thistle
+        Me.Button5.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(321, 652)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(173, 40)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "Mail"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(52, 156)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 19)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "COM Port:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(304, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 19)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "BAUD Rate:"
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Thistle
+        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(572, 652)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(173, 40)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Snapshot"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Calibri Light", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(1168, 658)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 19)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Timer"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.DustComm.My.Resources.Resources.dust_sensor_background1
+        Me.ClientSize = New System.Drawing.Size(1354, 733)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "DustComm"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ DustComm/Form1.vb
@@ -0,0 +1,179 @@
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+Imports System.Data.OleDb
+Imports EASendMail
+Public Class Form1
+    Dim portdata As String
+    Dim vArray As Array
+    Dim comOpen As String
+    Dim port As Array
+    Dim time As String
+    Dim time1 As String
+    Dim p_length As Integer
+    Dim tempChart As New Series
+    Dim readbuffer As String
+    Dim read As String
+    Dim provider As String
+    Dim datafile As String
+    Dim connstring As String
+    Dim cnn As New OleDbConnection
+    Dim cmd As New OleDbCommand
+    Dim userMsg As String
+    'Dim potChart As New Series
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        userMsg = Microsoft.VisualBasic.InputBox("Please Enter Password", "DustComm Password Manager", "Enter your password here", 500, 300)
+        If userMsg = "dustcomm7" Then
+
+        Else
+            MessageBox.Show("Wrong Password")
+            End
+        End If
+        Label5.Text = Format(Now, "General Date")
+        port = IO.Ports.SerialPort.GetPortNames()
+        ComboBox1.Items.AddRange(port)
+        p_length = port.Length
+        Timer1.Enabled = True
+        Chart1.Series.Clear()
+        Chart1.Titles.Add("Dust Monitoring")
+        tempChart.Name = "Dust Concentration (ug/m3)"
+        tempChart.ChartType = SeriesChartType.Line
+        Chart1.Series.Add(tempChart)
+        provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="
+        datafile = "D:\mydata.mdb"
+        connstring = provider & datafile
+        cnn.ConnectionString = connstring
+    End Sub
+    Private Sub SerialPort1_DataReceived(ByVal sender As System.Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        If comOpen Then
+            'System.Threading.Thread.Sleep(500)
+            readbuffer = SerialPort1.ReadExisting
+            'vArray = Split(readbuffer, ",")
+            Me.Invoke(New EventHandler(AddressOf updateTemp))
+        End If
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = CInt(ComboBox2.Text)
+
+        Try
+            SerialPort1.Open()
+            comOpen = SerialPort1.IsOpen
+        Catch ex As Exception
+            comOpen = False
+            MsgBox(ex.Message)
+
+        End Try
+        Button1.Enabled = False
+        Button2.Enabled = True
+        ComboBox1.Enabled = False
+        ComboBox2.Enabled = False
+
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        If comOpen Then
+            SerialPort1.DiscardInBuffer()
+            SerialPort1.Close()
+            comOpen = False
+            Button1.Enabled = True
+            Button2.Enabled = False
+            ComboBox1.Enabled = True
+            ComboBox2.Enabled = True
+
+        End If
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        TextBox1.Clear()
+        TextBox2.Clear()
+        ' ListBox1.ClearSelected()
+    End Sub
+    Public Sub updateTemp(ByVal sender As Object, ByVal e As System.EventArgs)
+        read = readbuffer.Replace(vbCr, "").Replace(vbLf, "")
+        TextBox1.Text = read
+        TextBox2.Text = "0001"
+        'TextBox2.Text = vArray(1)
+        Dim cnn As New OleDbConnection
+        Dim cmd As New OleDbCommand
+        provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="
+        datafile = "D:\mydata.mdb"
+        connstring = provider & datafile
+        cnn.ConnectionString = connstring
+        Try
+            cnn.Open()
+            cmd.Connection = cnn
+            cmd.CommandText = "Insert into Table1([time], [dust]) values(@time, @dust)"
+            cmd.Parameters.AddWithValue("@time", time1)
+            cmd.Parameters.AddWithValue("@Password", TextBox1.Text)
+            cmd.ExecuteNonQuery()
+            cnn.Close()
+            'MsgBox("Data added")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        tempChart.Points.AddXY(time, read)
+        'SerialPort1.DiscardInBuffer()
+        ' potChart.Points.AddXY(time, vArray(1))
+        ' ListBox1.Items.Insert(0, vArray(0))
+        'ListBox1.Items.Insert(1, time)
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ComboBox1.Items.Clear()
+        port = IO.Ports.SerialPort.GetPortNames()
+        ComboBox1.Items.AddRange(port)
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        port = IO.Ports.SerialPort.GetPortNames()
+        time = TimeOfDay
+        Label5.Text = Format(Now, "General Date")
+        time1 = Format(Now, "General Date")
+        If p_length <> port.Length Then
+            ComboBox1.Items.Clear()
+            ComboBox1.Items.AddRange(port)
+            p_length = port.Length
+        End If
+    End Sub
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Timer1.Enabled = False
+        'SerialPort1.DiscardInBuffer()
+        SerialPort1.Close()
+        comOpen = False
+        'ListBox1.ClearSelected()
+        End
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim oMail As New SmtpMail("TryIt")
+        Dim oSmtp As New SmtpClient()
+
+        oMail.From = "swainamit18sl@gmail.com"
+        oMail.To = "hbsahu@gmail.com"
+        oMail.Cc = "swainamit18is@gmail.com"
+        oMail.Subject = "First Mail"
+        oMail.AddAttachment("D:\mydata.mdb")
+        oMail.AddAttachment("D:\screenGrab.bmp")
+        oMail.TextBody = "This is a test email sent from VB.NET project with Gmail"
+
+        'Dim oServer As New SmtpServer("smtp.mail.yahoo.com")
+        Dim oServer As New SmtpServer("smtp.gmail.com")
+
+        oServer.User = "swainamit18sl@gmail.com"
+        oServer.Password = "ilsunnyleone"
+        oServer.Port = 465
+        oServer.ConnectType = SmtpConnectType.ConnectSSLAuto
+        Try
+            Console.WriteLine("start to send email over SSL ...")
+            oSmtp.SendMail(oServer, oMail)
+            Console.WriteLine("email was sent successfully!")
+            MsgBox("Mail has been Sent Successfully")
+        Catch ep As Exception
+            Console.WriteLine("failed to send email with the following error:")
+            Console.WriteLine(ep.Message)
+            MsgBox("Sending Failed!")
+        End Try
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim g As System.Drawing.Graphics = System.Drawing.Graphics.FromImage(screenGrab)
+        g.CopyFromScreen(New Point(0, 0), New Point(0, 0), screenSize)
+        screenGrab.Save("D:\screenGrab.bmp")
+    End Sub
+End Class
